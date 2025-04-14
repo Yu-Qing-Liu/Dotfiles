6384565cc526c9cd9929d63b68a3dcc354b6d3b9
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-04-14 12:50:58 -0400

    updates

--- dev/nvim/lua/plugins/lspconfig.lua
+++ dev/nvim/lua/plugins/lspconfig.lua
@@ -81,12 +81,23 @@ return {
     -- C++ LSP Settings
     lspconfig.clangd.setup {
       on_attach = lsp_attach,
-      capabilities = capabilities,
+      capabilities = {
+        offsetEncoding = { "utf-16" },
+      },
       cmd = {
         "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
         "--function-arg-placeholders",
         "--fallback-style=llvm",
-      }
+      },
+      init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
+      },
     }
 
     -- GLSL LSP Settings
