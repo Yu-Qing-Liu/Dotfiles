b7f9d0e9c59907108178132512107517153940b1
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-10-18 17:41:27 -0400

    power

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -47,6 +47,21 @@
   console.keyMap = "us";
   i18n.defaultLocale = "en_US.UTF-8";
 
+  # Battery Settings
+  services.system76-scheduler.settings.cfsProfiles.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_AC = 0;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
+  services.power-profiles-daemon.enable = false;
+  powerManagement.powertop.enable = false;
+  services.thermald.enable = true;
+
   # Printers
   services.printing = {
     enable = true;
