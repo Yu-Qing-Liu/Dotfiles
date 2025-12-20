c15528dba98eea8528e9ec0f44b424481f5c17f9
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2025-12-19 19:00:47 -0500

    monitors
    monitors
    
    monitors
    
    monitors
    
    monitors

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -1,136 +1,115 @@
 {
-    programs.waybar = {
-      enable = true;
-      systemd = {
-        enable = false;
-        target = "graphical-session.target";
-      };
-      style = builtins.readFile ./style.css;
-      settings = [{
-        "mode" = "dock";
-        "fixed-center" = false;
-        "name" = "bar1";
-        "output" = [
-          "eDP-1"
-          "DP-1"
-          "DP-2"
-        ];
-        "margin" = "7px 6px 0px 6px";
-        "layer" = "top";
-        "position" = "top";
-        "modules-left" = [
-          "hyprland/workspaces"
-        ];
-        "modules-right" = [
-          "custom/cpu-icon"
-          "cpu"
-          "custom/memory-icon"
-          "memory"
-          "custom/disk-icon"
-          "disk"
-          "custom/audio-icon"
-          "wireplumber"
-          "custom/keyboard-icon"
-          "hyprland/language"
-          "battery"
-          "bluetooth"
-          "custom/network"
-          "custom/date-icon"
-          "custom/date"
-          "custom/clock-icon"
-          "clock"
-          "custom/power"
-        ];
-        "hyprland/workspaces" = {
-          "format" = "{icon}";
-          "all-outputs" = true;
-          "disable-scroll" = true;
-        };
-        "custom/cpu-icon" = {
-          "format" = "";
-        };
-        "cpu" = {
-          "interval" = 5;
-          "format" = "{}%";
-          "max-length" = 10;
-        };
-        "custom/memory-icon" = {
-          "format" = "";
-        };
-        "memory" = {
-          "interval" = 5;
-          "format" = "{}%";
-          "max-length" = 10;
-        };
-        "custom/disk-icon" = {
-          "format" = "";
-        };
-        "disk" = {
-          "interval" = 30;
-          "format" = "{percentage_used}%";
-          "unit" = "GB";
-          "path" = "/";
-        };
-        "custom/audio-icon" = {
-          "format" = "";
-        };
-        "wireplumber" = {
-          "format" = "{volume}%";
-          "on-click" = "pavucontrol";
-          "max-volume" = 150;
-          "scroll-step" = 0.2;
-        };
-        "custom/keyboard-icon" = {
-          "format" = "";
-          "on-click" = "toggle_kb";
-        };
-        "hyprland/language" = {
-          "format" = "{short}";
-          "on-click" = "toggle_kb";
-        };
-        "battery" = {
-          "format" = "{icon}";
-          "format-alt" = "{icon}";
-          "format-alt-click" = "click-right";
-          "format-icons" = [
-            ""
-            ""
-            ""
-            ""
-          ];
-          "format-charging" = " ";
-          "interval" = 30;
-          "states" = {
-            "warning" = 45;
-            "critical" = 20;
-          };
-        };
-        "bluetooth" = {
-          "format" = "";
-        };
-        "custom/network" = {
-          "format" = "";
-          "on-click" = "alacritty -e impala";
-        };
-        "custom/clock-icon" = {
-          "format" = "";
-        };
-        "clock" = {
-          "interval" = 60;
-          "format" = "{:%I:%M %p}";
-        };
-        "custom/date-icon" = {
-          "format" = "";
-        };
-        "custom/date" = {
-          "interval" = 86400;
-          "exec" = "date +'%A, %B %d, %Y'";
-          "format" = "{}";
-        };
-        "custom/power" = {
-          "format" = "⏻ ";
-          "on-click" = "shutdown -h now";
-        };
-      }];
+  programs.waybar = {
+    enable = true;
+    systemd = {
+      enable = false;
+      target = "graphical-session.target";
     };
+    style = builtins.readFile ./style.css;
+    settings = [{
+      "mode" = "dock";
+      "fixed-center" = false;
+      "name" = "bar1";
+      "margin" = "7px 6px 0px 6px";
+      "layer" = "top";
+      "position" = "top";
+      "output" = [
+        "eDP-1"
+        "DP-5"
+        "DP-6"
+      ];
+      "modules-left" = [ "hyprland/workspaces" ];
+      "modules-right" = [
+        "custom/cpu-icon"
+        "cpu"
+        "custom/memory-icon"
+        "memory"
+        "custom/disk-icon"
+        "disk"
+        "custom/audio-icon"
+        "wireplumber"
+        "custom/keyboard-icon"
+        "hyprland/language"
+        "battery"
+        "bluetooth"
+        "custom/network"
+        "custom/date-icon"
+        "custom/date"
+        "custom/clock-icon"
+        "clock"
+        "custom/power"
+      ];
+      "hyprland/workspaces" = {
+        "format" = "{icon}";
+        "all-outputs" = true;
+        "disable-scroll" = true;
+      };
+      "custom/cpu-icon" = { "format" = ""; };
+      "cpu" = {
+        "interval" = 5;
+        "format" = "{}%";
+        "max-length" = 10;
+      };
+      "custom/memory-icon" = { "format" = ""; };
+      "memory" = {
+        "interval" = 5;
+        "format" = "{}%";
+        "max-length" = 10;
+      };
+      "custom/disk-icon" = { "format" = ""; };
+      "disk" = {
+        "interval" = 30;
+        "format" = "{percentage_used}%";
+        "unit" = "GB";
+        "path" = "/";
+      };
+      "custom/audio-icon" = { "format" = ""; };
+      "wireplumber" = {
+        "format" = "{volume}%";
+        "on-click" = "pavucontrol";
+        "max-volume" = 150;
+        "scroll-step" = 0.2;
+      };
+      "custom/keyboard-icon" = {
+        "format" = "";
+        "on-click" = "toggle_kb";
+      };
+      "hyprland/language" = {
+        "format" = "{short}";
+        "on-click" = "toggle_kb";
+      };
+      "battery" = {
+        "format" = "{icon}";
+        "format-alt" = "{icon}";
+        "format-alt-click" = "click-right";
+        "format-icons" = [ "" "" "" "" ];
+        "format-charging" = " ";
+        "interval" = 30;
+        "states" = {
+          "warning" = 45;
+          "critical" = 20;
+        };
+      };
+      "bluetooth" = { "format" = ""; };
+      "custom/network" = {
+        "format" = "";
+        "on-click" = "alacritty -e impala";
+      };
+      "custom/clock-icon" = { "format" = ""; };
+      "clock" = {
+        "interval" = 60;
+        "format" = "{:%I:%M %p}";
+      };
+      "custom/date-icon" = { "format" = ""; };
+      "custom/date" = {
+        "interval" = 86400;
+        "exec" = "date +'%A, %B %d, %Y'";
+        "format" = "{}";
+      };
+      "custom/power" = {
+        "format" = "⏻ ";
+        "on-click" = "shutdown -h now";
+      };
+    }];
+  };
 }
